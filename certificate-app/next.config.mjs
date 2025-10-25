63b396396933410e29e498602e68235ce4dda186
Vikkram SL <vikkram014@gmail.com>
2025-10-25 13:39:51 +0530

    first commit

--- certificate-app/next.config.mjs
+++ certificate-app/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["pino-pretty"],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
